6ffe2d927a205d478e092280e21cc174750e12ab
Nikhil <nikhil.kumaran.2011@gmail.com>
2017-08-05 12:28:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,4 @@
-$(".bounce").click(function() {
-    doBounce($(this), 3, '10px', 300);
-});
 
-
-function doBounce(element, times, distance, speed) {
-    for(i = 0; i < times; i++) {
-        element.animate({marginTop: '-='+distance},speed)
-            .animate({marginTop: '+='+distance},speed);
-    }
-}
 canvas = document.getElementsByTagName('canvas')[0];
 canvas.width = document.body.clientWidth;
 canvas.height = document.body.clientHeight;
