f0d82de177aa89281db39c2b495a20ea575f4193
Nikhil_Kumaran <nikhil.kumaran.2011@gmail.com>
2017-08-10 11:59:35 +0530

    Added 3D hover effect

--- scripts.js
+++ scripts.js
@@ -2,4 +2,9 @@ $(document).ready(function(){
     $('section').horizon();
     // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
     $('.modal').modal();
+    $(".logo").hover3d({
+      selector: ".logo__card",
+      shine: true,
+      sensitivity: 20,
+   });
 });
