de10603c232c9aee15054a2ffbdce3613eaa62fa
Akash Saravanan <akashsara@outlook.com>
2017-08-13 01:59:11 +0530

    Removed vertical scroll.

--- scripts.js
+++ scripts.js
@@ -1,6 +1,4 @@
 $(document).ready(function(){
-    $('section').horizon();
-    // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
     $('.modal').modal();
     $(".logo").hover3d({
       selector: ".logo__card",
