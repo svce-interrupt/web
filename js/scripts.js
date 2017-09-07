caa7b0b928c4d80ca4d18a772d503171390100e8
Akash Saravanan <akashsara@outlook.com>
2017-09-07 12:15:55 +0530

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,72 @@
+window.sr = ScrollReveal();
+
+sr.reveal('.fromLeft',{
+    duration: 1300,
+    origin:'left',
+    easing:'cubic-bezier(1,-0.03, 0.15, 1.23)',
+    distance: '100px'
+});
+
+sr.reveal('.fromRight',{
+    duration: 1300,
+    origin:'right',
+    easing:'cubic-bezier(1,-0.03, 0.15, 1.23)',
+    distance: '100px'
+});
+
+sr.reveal('.item',{
+    duration: 1300,
+    easing:'cubic-bezier(1,-0.03, 0.15, 1.23)',
+    origin: 'bottom',
+    scale: 0.001
+});
+
+sr.reveal('.logo',{
+    duration: 1000,
+    origin:'bottom',
+    scale:0.0001,
+    easing:'cubic-bezier(1,-0.03, 0, 2.31)',
+    delay:500
+});
+
+$(document).ready(function(){
+  $('.modal').modal({
+    ready: function () {
+        $('ul.tabs').tabs();
+    }
+  });
+
+	$(".button-collapse").sideNav({
+      closeOnClick: true,
+      draggable: true,
+  });
+
+  $(".logo").hover3d({
+    selector: ".logo__card",
+		shine: true,
+		sensitivity: 20,
+  });
+
+  $('.btn-floating').click(function(e) {
+    var link = $(this).attr('href');
+    //console.log($(link).offset().top);
+    $('html, body').animate({
+        scrollTop: $(link).offset().top
+    }, 900);
+    e.preventDefault();
+  });
+
+  typer('.typed-text')
+    .line()
+    .pause(1000)
+    .continue('September 25th.', 200)
+    .pause(3000)
+    .back(15, 100)
+    .continue('Be There.', 200)
+    .pause(1000)
+    .back(15, 100)
+    .continue('September 25th.', 200)
+    .pause(1000)
+    .end();
+
+});
