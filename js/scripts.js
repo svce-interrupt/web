cf5490b1c3c1d35163a72dafde80aa9f3cbe9598
Akash Saravanan <akashsara@outlook.com>
2017-09-08 13:32:33 +0530

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -29,41 +29,57 @@ sr.reveal('.logo',{
     delay:500
 });
 
+function myMap() {
+    var mapCanvas = document.getElementById("map");
+    var college = new google.maps.LatLng(12.987056, 79.972100);
+    var collegeMarker = new google.maps.Marker({position:college});
+    var csDept = new google.maps.LatLng(12.987496, 79.972795);
+    var csDeptMarker = new google.maps.Marker({position:csDept});
+    var canteen = new google.maps.LatLng(12.986547, 79.972199);
+    var canteenMarker = new google.maps.Marker({position:canteen});
+    var mapOptions = {center: college, zoom: 17};
+    var map = new google.maps.Map(mapCanvas, mapOptions);
+    collegeMarker.setMap(map);
+    canteenMarker.setMap(map);
+    csDeptMarker.setMap(map);
+}
+
 $(document).ready(function(){
-  $('.modal').modal({
-    ready: function () {
-        $('ul.tabs').tabs();
-    }
-  });
+    $('.modal').modal({
+        ready: function () {
+            $('ul.tabs').tabs();
+        }
+    });
 
-	$(".button-collapse").sideNav({
-      closeOnClick: true,
-      draggable: true,
-  });
+    $(".button-collapse").sideNav({
+        closeOnClick: true,
+        draggable: true,
+    });
 
-  $(".logo").hover3d({
-    selector: ".logo__card",
-		shine: true,
-		sensitivity: 20,
-  });
+    $(".logo").hover3d({
+        selector: ".logo__card",
+        shine: true,
+        sensitivity: 20,
+    });
 
-  $('.btn-floating').click(function(e) {
-    var link = $(this).attr('href');
-    $('html, body').animate({
-        scrollTop: $(link).offset().top
-    }, 900);
-    e.preventDefault();
-  });
+    $('.btn-floating').click(function(e) {
+        var link = $(this).attr('href');
+        $('html, body').animate({
+            scrollTop: $(link).offset().top
+        }, 900);
+        e.preventDefault();
+    });
 
-  $('.nav-scroll').click(function(e) {
-    var link = $(this).attr('href');
-    $('html, body').animate({
-        scrollTop: $(link).offset().top
-    }, 900);
-    e.preventDefault();
-  });
+    $('.nav-scroll').click(function(e) {
+        var link = $(this).attr('href');
+        $('html, body').animate({
+            scrollTop: $(link).offset().top
+        }, 900);
+        e.preventDefault();
+    });
 
-  typer('.typed-text')
+    /*Typed Text*/
+    typer('.typed-text')
     .line()
     .pause(1000)
     .continue('September 25th.', 200)
@@ -76,6 +92,7 @@ $(document).ready(function(){
     .pause(1000)
     .end();
 
+    /*Registration Form*/
     $("#reg-form").validate({
         rules: {
             fname: {
@@ -100,51 +117,54 @@ $(document).ready(function(){
                 required: true,
                 email:true
             },
-            phone: {
+            mobile_num: {
                 required: true,
-                minlength: 10
+                minlength: 10,
+                number: true
             },
             pass: {
                 required: true,
-                minlength: 5
+                minlength: 6
             },
             confirm_pass: {
                 required: true,
-                minlength: 5,
+                minlength: 6,
                 equalTo: "#pass"
             }
         },
-        //For custom messages
         messages: {
             fname: {
                 required: "Please enter your first name.",
                 minlength: "You sure you're named with one letter?"
             },
             lname: {
-                required: "Please enter your last name.",
-                minlength: "You sure you're named with one letter?"
+                required: "Please enter your last name."
             },
             email: {
                 required: "Please enter your email address.",
                 email: "Please enter a valid email address."
             },
-            phone: {
+            col: {
+                required: "Please enter your college name."
+            },
+            mobile_num: {
                 required: "Please enter your phone number.",
+                number: "That isn't even a number!",
                 minlength: "That doesn't seem like a phone number to me. :/"
-            }.
+            },
             pass: {
                 required: "Please enter a password.",
-                minlength: "Password must be atleast 5 characters."
+                minlength: "Password must be atleast 6 characters."
             },
             confirm_pass: {
                 required: "Please confirm your password.",
-                minlength: "Password must be atleast 5 characters.",
+                minlength: "Password must be atleast 6 characters.",
                 equalTo: "Password does not match."
             }
         },
         errorPlacement: function(error, element) {
-            error.appendTo( element.next() );
+            element.next().remove();
+            error.insertAfter(element);
         }
-
     });
 });
